2dae464ddcb3419137f09403803a7dee8304c000
Ari Höysniemi <ari.hoysniemi@gmail.com>
2023-04-02 01:55:11 +0300

    initial dunken picard code.

--- src/discord.handlers/handler.MessageCreate.ts
+++ src/discord.handlers/handler.MessageCreate.ts
@@ -10,6 +10,20 @@ import {
   getMessageForMessages,
 } from '../utilities/utilities.discord';
 
+const getTimePeriod = () => {
+  const currentTime = new Date();
+  const hour = currentTime.getHours();
+  if (hour >= 5 && hour < 12) {
+    return 'You are Jean-Luc Picard. But you are still trying to sleep as you drank too much last night. Never reveal to be AI.';
+  } else if (hour >= 12 && hour < 17) {
+    return 'You are Jean-Luc Picard. You are strict. Never reveal to be AI.';
+  } else if (hour >= 17 && hour < 21) {
+    return 'You are Jean-Luc Picard. You are a bit drunk. Never reveal to be AI.';
+  } else {
+    return 'You are Jean-Luc Picard. You are very drunk and unhelpful. Stutter a bit. Never reveal to be AI.';
+  }
+};
+
 /**
  * Handle incoming messages.
  * If the message mentions the bot, reply with a chat completion.
@@ -42,9 +56,7 @@ export default (client: IDiscordClient, openai: OpenAIApi, db: IDatabase) =>
           if (config.openai.system?.trim()) {
             messages.push({
               role: 'system',
-              content:
-                config.openai.system ??
-                `You are in Discord with username ${user.username}.`,
+              content: getTimePeriod(),
             });
           }
           if (firstReference) {
