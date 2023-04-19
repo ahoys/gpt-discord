a5417369656e3830f44cd16e0a921a812dc8752b
Ari Höysniemi <ari.hoysniemi@gmail.com>
2023-04-19 15:08:20 +0300

    Max content length doubled for discord.

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ const production = {
     appId: process.env.DISCORD_APPID,
     token: process.env.DISCORD_TOKEN,
     role: process.env.DISCORD_ROLEID,
-    maxContentLength: Number(process.env.DISCORD_MAXCONTENTLENGTH) || 2000,
+    maxContentLength: Number(process.env.DISCORD_MAXCONTENTLENGTH) || 4000,
     ignoreCommands: process.env.DISCORD_IGNORECOMMANDS?.split(',') || [],
     customCommandsDir: process.env.DISCORD_CUSTOMCOMMANDSDIR,
   },
